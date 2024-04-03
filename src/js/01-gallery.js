e61a3b6e27676ccd5021f96a8fc823cd739c1654
Valerii Arlamovskyi <valera.arl96@gmail.com>
2024-04-02 21:08:28 -0400

    second commit

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+
+const markup = galleryItems.map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+      <img
+        class="gallery__image"
+        src="${preview}"
+        alt="${description}"
+        title="${description}"
+      />
+    </a>
+    </li>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", markup);
+const options =  {
+    captions:'true',
+    captionDelay:'1000',
+  };
+new SimpleLightbox(".gallery__link", options);
