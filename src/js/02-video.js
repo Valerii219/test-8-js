06e870ef0aaaea16a64281f83f7247b8bfe9b4e2
Valerii Arlamovskyi <valera.arl96@gmail.com>
2024-04-04 21:56:13 -0400

    add js to video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
-// console.log(2);
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+
+const KEYFORVIDEO = 'videoplayer-current-time';
+const player = new Player(iframe);
+function local(data) {
+  const time = data.seconds;
+  localStorage.setItem(KEYFORVIDEO, JSON.stringify(time));
+}
+
+player.on('timeupdate', throttle(local, 1000));
+const parse = JSON.parse(localStorage.getItem(KEYFORVIDEO)) || 0;
+player.setCurrentTime(parse);
